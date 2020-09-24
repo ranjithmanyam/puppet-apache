f0773229270936ae376c2b67516d70459656c2df
root <root@ranjithmanyam1c.mylabserver.com>
2020-09-24 19:36:21 +0000

    Refactored install class for Debian family servers

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,10 +1,12 @@
-# @summary 
+# @summary
 #
-# Installs the base apache package 
+# Installs the base apache package
 #
-class apache::install {
-  
-  package { 'httpd': 
-    ensure => present,
-  } 
+class apache::install (
+  $install_name = $apache::params::install_name,
+  $install_ensure = $apache::params::install_ensure,
+) inherits apache::params {
+  package { "${install_name}":
+    ensure => $install_ensure,
+  }
 }
